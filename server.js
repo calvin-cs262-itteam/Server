35c7bd8ef6c6585fa9cb5bb6bb56aae9a9343b6a
unknown <njw32@students.calvin.edu>
2019-11-07 23:16:53 -0500

    get, post, put, delete

--- server.js
+++ server.js
@@ -37,6 +37,66 @@ app.use(function(req, res, next)
     next();
 });
 
+apiRouter.get('/EasyEats', function(req, res){
+    database.find({ displayed: true}, (err, recs) =>
+    {
+        if (err){
+            console.dir(err);
+        }   
+        res.json({records: recs});
+    });
+});
+
+apiRouter.post('/EasyEats', function(req, res){
+    var name = req.body.name,
+    description = req.body.description;
+
+    database.create({name: name,
+            description: description},
+        function (err, small)
+    {if (err){
+        console.dir(err);
+    }
+    res.json({message: 'success'});
+
+    });
+
+});
+
+apiRouter.put('/EasyEats/:recipeID', function(req, res){
+    database.findById({
+        _id: req.params.recordID}, (err, recs) =>
+        {
+            if(err){
+                console.dir(err);
+            }
+            else{
+                recs.name = req.body.name || recs.name;
+                recs.description = req.body.description || recs.description;
+
+                recs.save((err, recs) =>
+                {if(err){
+                    res.status(500).send(err)
+                }
+                res.json({records:recs});
+            });
+        }
+        });
+
+});
+
+apiRouter.delete('/EasyEats/:recipeID', function(req, res){
+    database.findByIdAndRemove({_id: req.params.recordID}, (err, recs) =>
+    {
+        if (err)
+        {
+            console.dir(err);
+        }
+        res.json({records: recs});
+    });
+
+});
+
 
 app.use('/api', apiRouter);
 
